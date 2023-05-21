f0f076d2eb9b6e88a1d7d7ced66aaaf23fc99a43
ZaidSattar <131235332+ZaidSattar@users.noreply.github.com>
2023-05-21 11:03:25 -0400

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,5 @@
-// Variable to store the toggle state
 var isEnabled = false;
 
-// Message listener to handle message passing between content script and background script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "getToggleState") {
     sendResponse({ isEnabled: isEnabled });
@@ -11,11 +9,36 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
-// Initialize the toggle state based on the saved state in chrome.storage
 chrome.storage.sync.get("owlPlusEnabled", function(result) {
   isEnabled = result.owlPlusEnabled;
 });
 
-// Save and retrieve the toggle state using chrome.storage if needed
 chrome.storage.sync.set({ owlPlusEnabled: isEnabled });
 
+
+console.log("Toggle state:", isEnabled);
+
+function sendMessageToContentScript() {
+  var message = {
+    action: "showNotification",
+    message: "Hello from the background script!"
+  };
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+      console.log("Response from content script:", response);
+    });
+  });
+}
+
+function listenForToggleStateChanges() {
+  // Simulating a toggle state change
+  isEnabled = !isEnabled;
+
+  chrome.storage.sync.set({ owlPlusEnabled: isEnabled });
+
+  console.log("Updated toggle state:", isEnabled);
+}
+
+sendMessageToContentScript();
+listenForToggleStateChanges();
